7e1f095a7e4df7931cac12071e564603c9857701
Thomas Francart <thomas.francart@sparna.fr>
2024-03-05 18:26:31 +0100

    Fix menu links opening in new windows + backlink on references page

--- src/assets/js/sparna.js
+++
@@ -1,9 +0,0 @@
-var url = "http://www.sparna.fr";
-jQuery(document).ready(function(){
-	jQuery("#menu a").click(function(e){
-		if (jQuery(this).attr("href").substring(0, 20) != url){
-			window.open(jQuery(this).attr("href"), "_blank");
-			e.preventDefault();
-		}
-	})
-})
