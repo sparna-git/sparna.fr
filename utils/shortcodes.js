4c7c4a1543a446c0d92ab138ba3dfd127c43641f
Thomas Francart <thomas.francart@sparna.fr>
2025-02-23 18:27:22 +0100

    fix goatcounter

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -2,8 +2,7 @@ module.exports = {
 
     analytics: function () {
         return process.env.ENVIRONMENT === "prod"
-          ?`
-    <script data-goatcounter="https://sparna.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
+          ?`<script data-goatcounter="https://sparna.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>`
           : "";
     }
     
