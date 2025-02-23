581ffcaa57570cffc1be826edb3a6a87279c038d
Thomas Francart <thomas.francart@sparna.fr>
2025-02-23 18:25:58 +0100

    Switched to GoatCounter

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -3,13 +3,7 @@ module.exports = {
     analytics: function () {
         return process.env.ENVIRONMENT === "prod"
           ?`
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-6K99SH9JG6"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-6K99SH9JG6');
-</script>`
+    <script data-goatcounter="https://sparna.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
           : "";
     }
     
